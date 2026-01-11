3e07650ff3a28f72f2b60cc59ac5192f00b3deea
Zispiro25 <martincornejoperez25@gmail.com>
2026-01-10 23:25:57 -0300

    añadido funcionalidades

--- controlador.py
+++ controlador.py
@@ -1,25 +1,52 @@
 from flask import Flask, render_template, request
-from prender_s import prender_server
+import requests
 app = Flask(__name__)
-
+from prender_s import prender_server
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('password.html')
+
 @app.route("/verificacion_sesion", methods = ['GET'])
 def verificacion_sesion():
     return render_template('password.html')
+
 @app.route("/iniciar_sesion", methods = ['POST'])
 def iniciar_sesion():
-    #esto es de prueba, en prod eliminar y usar un json o algo
     nombre = request.form['usuario']
     contra = request.form['contra']
+    request.cookies.set("username", nombre)
+    
     if nombre == "admin" and contra == "123":
         return render_template('main.html')
     else:
         return 'contraseña/usuario incorrecto'
-@app.route("main/prender", methods=['POST'])
-def orden():
-    prender_server()
-    return render_template('main.html')
-
     
+@app.route("/prender", methods=['GET'])
+def prender():
+    estado = "activado"
+    prender = prender_server()
+    if(prender):
+        return {"estado" : estado}
+    else:
+        estado = "error"
+        return {"estado" : estado}
+    
+@app.route("/apagar_server", methods=['GET'])
+def apagar():
+    try:
+        response = requests.get("http://192.168.100.13:5000/apagar", timeout = 10)
+    except requests.exceptions.Timeout:
+        return {"estado":"apagado"}
+    finally:
+       return {"estado" : "apagado"}
+    
+
+@app.route("/status_server", methods=['GET'] )
+def status():
+    try:
+        response = requests.get("http://192.168.100.13:5000/status", timeout=5)
+        respuesta = response.json()
+    except requests.exceptions.Timeout:
+        return {"estado":"apagado"}
+    if(respuesta["estado"] == "prendido"):
+        return {"estado" : "prendido"}
--- prender_s.py
+++ prender_s.py
@@ -1,4 +1,8 @@
 from wakeonlan  import send_magic_packet
 
 def prender_server():
-    send_magic_packet('00:e0:0b:58:41:08')
+    try:
+        send_magic_packet('00:e0:0b:58:41:08',ip_address='192.168.100.13')
+        return True
+    except ValueError as e:
+        return False
